b85113c8cd44c039d1f61ac865cfc7fbcc8741e2
isaatsu <atsushi.isai131@gmail.com>
2021-04-21 22:30:14 +0900

    createElement(

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  li.appendChild(div);
+  div.appendChild(p);
+
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document
