dd9b8e15760be6454b93c189c2980d9afb5cd041
isaatsu <atsushi.isai131@gmail.com>
2021-04-22 14:27:24 +0900

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  const li = document.createElement("li");
+  // const li = document.createElement("li");
 
   const div = document.createElement("div");
   div.className = "list-row";
